54769e0b2e707a2defc3b82fca4a406c620c061a
igor-ribeir0 <igor_ribeirosantos@outlook.com>
2022-10-29 15:36:22 -0300

    versão finalizada completa

--- JAVASCRIPT/script.js
+++ JAVASCRIPT/script.js
@@ -35,8 +35,12 @@ pegarDados();
 
 function dadosSucesso(resposta){
     const elemento = document.querySelector("main");
+    const elementoAparacer = document.querySelector(".caixaMensagem");
     elemento.innerHTML = "";
-    elemento.classList.remove("escondido");
+
+    if(nome !== ""){
+        elemento.classList.remove("escondido");
+    }
 
     for(let i = 0; i < resposta.data.length; i++){
         let item = `
@@ -47,11 +51,48 @@ function dadosSucesso(resposta){
         </div>`
 
         elemento.innerHTML += item;
-        elemento.scrollIntoView();
+        elementoAparacer.scrollIntoView();
     }
 }
 setInterval(pegarDados, 3000);
 
 function dadosFalha(erro){
-    alert("deu ruim");
+    const statusCode = erro.response.status;
+
+    if(statusCode === 301){
+        alert("O recurso que você está tentando acessar foi movido pra outra URL");
+    }
+    else if(statusCode === 401){
+        alert("Você não tem acesso a esse recurso");
+    }
+    else if(statusCode === 404){
+        alert("O recurso pedido não existe");
+    }
+    else if(statusCode === 409){
+        alert("O recurso que você está tentando inserir já foi inserido");
+    }
+    else if(statusCode === 422){
+        alert("A requisição enviada não está no formato esperado");
+    }
+    else if(statusCode === 500){
+        alert("Ocorreu algum erro desconhecido no servidor");
+    }
+}
+
+function enviar(){
+    let mensagem = document.querySelector("input");
+    const mandar = axios.post("https://mock-api.driven.com.br/api/v6/uol/messages",{
+        from: nome,
+        to: "Todos",
+        text: mensagem.value,
+        type: "message",
+    });
+    mandar.then(pegarDados);
+    mandar.catch(atualizarSala);
+    mensagem.value = "";
+}
+
+function atualizarSala(){
+    alert("Usuário desconectado!A página será atualizada.");
+    window.location.reload();
 }
